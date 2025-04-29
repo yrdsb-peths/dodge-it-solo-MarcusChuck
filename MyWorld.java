601bfc3605832449e0d3e8941cc923e3753d3a32
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-04-29 08:24:27 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
+        
+        Spiderman fish = new Spiderman();
+        addObject(fish, 600, 100);
     }
 }
