46792456d478a726f1f11b5b053794cc95571b62
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-04-29 08:41:39 -0400

    resets spiderman if he hits hero and added horizontal movement

--- Spiderman.java
+++ Spiderman.java
@@ -1,19 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Spiderman here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Spiderman extends Actor
 {
-    /**
-     * Act - do whatever the Spiderman wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
+        move(-10);
+        
+        if(getX() <= 0) {
+            resetSpiderman();
+        }
+        
+        if(isTouching(Hero.class))
+        {
+            getWorld().removeObject(this);
+        }
+    
         // Add your action code here.
     }
+    
+    public void resetSpiderman() {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0) {
+            setLocation(600,100);
+        }else{
+            setLocation(600,300);
+        }
+        }
 }
+
